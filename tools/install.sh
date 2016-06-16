40b2eba52dede912f2185df5b8bf7d24a65e6fd2
Sir William Tatcher <willfelix93@gmail.com>
2016-06-15 21:02:01 -0300

    merge

--- tools/install.sh
+++ tools/install.sh
@@ -1,2 +1,4 @@
 curl https://raw.githubusercontent.com/WillFelix/Pushit/master/bin/pushit > pushit
 sudo mv pushit /usr/local/bin/
+sudo chmod 777 /usr/local/bin/pushit
+printf "\n\nPushit installed successfully.\n\n"
