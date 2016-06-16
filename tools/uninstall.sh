d7c9882ea89e3a04d4c6219bd822c1f29383ce34
Sir William Tatcher <willfelix93@gmail.com>
2016-06-16 09:40:46 -0300

    merge

---
+++ tools/uninstall.sh
@@ -0,0 +1,13 @@
+sudo rm /usr/local/bin/pushit
+
+echo ""
+echo " ____________________ "
+echo "< Pushit has gone ;( >"
+echo " -------------------- "
+echo "        \\   ^__^"
+echo "         \\  (oo)\\_______"
+echo "            (__)\\       )\\/\\"
+echo "                ||----w |"
+echo "                ||     ||"
+echo ""
+echo ""
