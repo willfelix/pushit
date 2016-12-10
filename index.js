1e9dd4aac9fa303a9d10f78366b2d5206ea54c88
Will Felix <willfelix93@gmail.com>
2016-12-10 17:20:52 -0300

    Merge

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ program.parse(process.argv);
 
   exec("git branch | grep '* ' | sed -e 's/* //g'", function(err, stdout, stderr) {
     if (!program.branch) {
-      console.log(stdout.replace("/n", ""));
+      console.log(stdout.replace("\n", ""));
       console.log(stdout);
       current_branch = stdout || current_branch;
     }
